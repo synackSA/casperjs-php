b54fec8e02988ee388630a3c882a6985f9222760
synacksa <syndrome@gmail.com>
2016-08-22 11:39:46 -0400

    Adding class

---
+++ src/Casper.php
@@ -0,0 +1,39 @@
+<?php
+namespace Browser;
+
+/**
+ * CasperJS wrapper
+ *
+ * @author Garth Michel <syndrome@gmail.com>
+ *
+ */
+class Casper extends \Browser\Casper
+{
+
+    /**
+     * Gets the current cookies from the session
+     *
+     * @return \Browser\Casper
+     * @author  Garth Michel
+     */
+    public function saveCookies($cookieFile)
+    {
+        $fragment = <<<FRAGMENT
+casper.then(function () {
+var fs = require('fs');
+var file = fs.open('$cookieFile', {
+    mode: 'w'
+});
+file.write(JSON.stringify(phantom.cookies));
+file.close();
+});
+FRAGMENT;
+
+        $this->_script .= $fragment;
+
+        return $this;
+
+    }
+
+
+}
